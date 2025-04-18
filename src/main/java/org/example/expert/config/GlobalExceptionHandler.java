505ef66f8147c5af457987d5fde3c6935315e8a2
mxcoogi <mxcoogi@gmail.com>
2025-04-18 10:48:56 +0900

    lv1-3 : Validation service 검증 -> controller 검증 GlobalExceptionHandler에 예외처리 추가

--- src/main/java/org/example/expert/config/GlobalExceptionHandler.java
+++ src/main/java/org/example/expert/config/GlobalExceptionHandler.java
@@ -5,6 +5,8 @@ import org.example.expert.domain.common.exception.InvalidRequestException;
 import org.example.expert.domain.common.exception.ServerException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -30,6 +32,15 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Map<String, Object>> handleServerException(ServerException ex) {
         HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
         return getErrorResponse(status, ex.getMessage());
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidException(MethodArgumentNotValidException ex){
+        Map<String, String> map = new HashMap<>();
+        for(FieldError error : ex.getBindingResult().getFieldErrors()){
+            map.put(error.getField(), error.getDefaultMessage());
+        }
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        return getErrorResponse(status, map.toString());
     }
 
     public ResponseEntity<Map<String, Object>> getErrorResponse(HttpStatus status, String message) {
